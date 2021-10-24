b924a8c6d380011080ea637e7b66e5346fde24d5
mrash13 <yaro.fury@gmail.com>
2021-10-24 20:45:29 +0300

    Update Wallet.sol

--- Wallet_1/Wallet.sol
+++ Wallet_1/Wallet.sol
@@ -22,18 +22,28 @@ contract Wallet {
         // Runtime function that obtains sender's public key.
         require(msg.pubkey() == tvm.pubkey(), 100);
 
-		// Runtime function that allows contract to process inbound messages spending
-		// its own resources (it's necessary if contract should process all inbound messages,
-		// not only those that carry value with them).
-		tvm.accept();
-		_;
-	}
+        // Runtime function that allows contract to process inbound messages spending
+        // its own resources (it's necessary if contract should process all inbound messages,
+        // not only those that carry value with them).
+        tvm.accept();
+        _;
+    }
 
     // Отправить без оплаты комиссии за свой счет
     function sendWithoutFee(address dest, uint128 amount) public pure checkOwnerAndAccept {
         dest.transfer(amount, true, 0);
     }
 
+    // Отправить с оплатой комиссии за свой счет
+    function sendWithFee(address dest, uint128 amount) public pure checkOwnerAndAccept {
+        dest.transfer(amount, true, 1);
+    }
+
+    // Отправить все деньги и уничтожить кошелек
+    function sendAllAndDestroyAcc(address dest, uint128 amount) public pure checkOwnerAndAccept {
+        dest.transfer(amount, true, 128 + 32);
+    }
+}
     // Отправить с оплатой комиссии за свой счет
     function sendWithFee(address dest, uint128 amount) public pure checkOwnerAndAccept {
         dest.transfer(amount, true, 1);
