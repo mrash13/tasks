911e4aa6ea603f69b59af4561f237ffa5946a038
mrash13 <yaro.fury@gmail.com>
2021-10-25 11:38:51 +0300

    Add files via upload

--- SimpleToken/SimpleToken.sol
+++ SimpleToken/SimpleToken.sol
@@ -1,18 +1,24 @@
 pragma ton-solidity >= 0.35.0;
 pragma AbiHeader expire;
 
-// This is class that describes you smart contract.
 contract SimpleToken {
 
+    /* Exit коды:
+        103 - не владелец токена
+        104 - токена с таким именем не существует
+        105 - токен с таким именем уже существует
+    */
+    
     struct PlayerToken {
         string nickname;
         string teamName;
         string position;
         uint   tokenOwner;
+        uint   price;
     }
 
     mapping(string => PlayerToken) players;
-    mapping(string => uint) tokenPrices;
+    uint tOwner;
 
     constructor() public {
         // check that contract's public key is set
@@ -22,22 +28,33 @@ contract SimpleToken {
         tvm.accept();
     }
 
-    modifier onlyByTokenOwner(uint _owner) {
-        require(msg.pubkey() == _owner, 103);
+    modifier onlyByTokenOwner(uint _tOwner) {
+        require(msg.pubkey() == _tOwner, 103);
+        _;
+    }
+
+    modifier onlyIfTokenExists(string _tName) {
+        require(players.exists(_tName), 104);
         _;
     }
 
     function createPlayerToken(string _nickname, string _teamName, string _position) public {
-        require(!players.exists(_nickname), 104);
+        require(!players.exists(_nickname), 105);
         tvm.accept();
         players[_nickname].nickname = _nickname;
         players[_nickname].teamName = _teamName;
         players[_nickname].position = _position;
         players[_nickname].tokenOwner = msg.pubkey();
+        tOwner = players[_nickname].tokenOwner;
     }
 
-    function putOnSale(string _nickname, uint price) public onlyByTokenOwner(players[_nickname].tokenOwner){
+    function putOnSale(string _nickname, uint _price) public onlyByTokenOwner(tOwner) onlyIfTokenExists(_nickname) {
         tvm.accept();
-        tokenPrices[_nickname] = price;
+        players[_nickname].price = _price;
+    }
+
+    function getTokenPrice(string _nickname) public onlyIfTokenExists(_nickname) view returns(uint) {
+        tvm.accept();
+        return players[_nickname].price;
     }
 }
